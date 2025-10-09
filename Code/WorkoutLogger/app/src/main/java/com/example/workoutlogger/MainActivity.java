fdb3eef5715f84c47d2b8a2d11066c22d7e460c1
tim <tschneid37@gmail.com>
2025-10-09 15:54:31 -0700

    Emulator should work now hopefully

--- Code/WorkoutLogger/app/src/main/java/com/example/workoutlogger/MainActivity.java
+++ Code/WorkoutLogger/app/src/main/java/com/example/workoutlogger/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
